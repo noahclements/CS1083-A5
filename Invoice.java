562d512321afad053a8dc65f4f43320120eb5b0b
Noah Clements <noahnoah900@gmail.com>
2019-03-28 00:41:03 -0300

    WIP

--- Invoice.java
+++ Invoice.java
@@ -1,5 +1,16 @@
-public class Invoice {
-	
+public class Invoice implements Comparable<Invoice>{
+	private int customerID;
+	private int invoiceID;
+	private double amount;
 
+	public Invoice(int custIn, int invIn, double amtIn) {
+		customerID = custIn;
+		invoiceID = invIn;
+		amount = amtIn;
+	}
+
+	public int compareTo(Invoice other) {
+		
+	} 
 	
 }

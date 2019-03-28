0bd4d26899de3dcd4a5badf2a60c3035a9bd2161
Noah Clements <noahnoah900@gmail.com>
2019-03-28 12:19:14 -0300

    adding methods + fixed bug
    changed int to Invoice

--- InvoiceLinkedList.java
+++ InvoiceLinkedList.java
@@ -9,7 +9,7 @@ public class InvoiceLinkedList {
 	}
 
 
-	public int getSize() {
+	public int size() {
 		int counter = 0;
 		Node curr = head;
 		while(curr != head) {
@@ -44,7 +44,6 @@ public class InvoiceLinkedList {
 		} else {
 			Node curr = head;
 			Node prev = head;
-
 			while(curr != null && curr.data < addNode.data) {
 				prev = curr;
 				curr = curr.next;
@@ -54,8 +53,35 @@ public class InvoiceLinkedList {
 			addNode.prev = prev;
 			addNode.next = curr;
 		}
+		size++;
+	}
+
+	public Invoice get(int index) {
+		Node curr = head;
+		int count = 1;
+		while(count < index) {
+			curr = curr.next;
+			count++;
+		}
+		return curr.data;
+	}
+
+	public boolean remove(Invoice invoice) {
+		Node removeNode = new Node(invoice);
+		Node curr = head;
+		while(curr != null || curr != removeNode) {
+			curr = curr.next;
+		}
 
 	}
+
+	public void swapValues(int index1, int index2) {
+
+	}
+
+
+
+
 
 
 	public void print() {
@@ -68,18 +94,19 @@ public class InvoiceLinkedList {
 
 
 	public class Node { // inner
-		public int data;
+		public Invoice data;
 		public Node prev,next;
 
-		public Node(int dataIn) {
+		public Node(Invoice dataIn) {
 			data = dataIn;
 			next = null;
 			prev = null;
 		}
 
-		public Node(int data, Node next) { // overload
-			this.data = data;
-			this.next = next;
+		public Node(Invoice dataIn, Node nextIn) { // overload
+			data = dataIn;
+			next = nextIn;
+
 		}
 	}
 
